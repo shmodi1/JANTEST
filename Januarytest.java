9e2c859d0e96119a7f0fd3f5b27566a6862326ac
shmodi1 <shalini.modi@copart.com>
2023-01-12 19:58:06 -0600

    Update README.md

---
+++ Januarytest.java
@@ -0,0 +1,12 @@
+public class Januarytest {
+    public static void main(String[] args)
+
+    {
+        System.out.println("java project");
+        System.out.println("java project1");
+
+        int x = 5;
+        int y = 6;
+        System.out.println(x + y);
+    }
+}
